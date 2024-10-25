4be65e4369664f7c74de714fc5187bc630961fa2
MithilMestry <mestrymithil@gmail.com>
2024-10-26 03:20:01 +0530

    sign-up page done

--- src/components/auth/layout.tsx
+++ src/components/auth/layout.tsx
@@ -24,7 +24,7 @@ export function Layout({ children }: LayoutProps): React.JSX.Element {
       <Box sx={{ display: 'flex', flex: '1 1 auto', flexDirection: 'column' }}>
         <Box sx={{ p: 3 }}>
           <Box component={RouterLink} href={paths.home} sx={{ display: 'inline-block', fontSize: 0 }}>
-            <DynamicLogo colorDark="light" colorLight="dark" height={32} width={122} />
+            {/* <DynamicLogo colorDark="light" colorLight="dark" height={32} width={122} /> */}
           </Box>
         </Box>
         <Box sx={{ alignItems: 'center', display: 'flex', flex: '1 1 auto', justifyContent: 'center', p: 3 }}>
@@ -42,7 +42,7 @@ export function Layout({ children }: LayoutProps): React.JSX.Element {
         }}
       >
         <Stack spacing={3}>
-          <Stack spacing={1}>
+          {/* <Stack spacing={1}>
             <Typography color="inherit" sx={{ fontSize: '24px', lineHeight: '32px', textAlign: 'center' }} variant="h1">
               Welcome to{' '}
               <Box component="span" sx={{ color: '#15b79e' }}>
@@ -52,13 +52,13 @@ export function Layout({ children }: LayoutProps): React.JSX.Element {
             <Typography align="center" variant="subtitle1">
               A professional template that comes with ready-to-use MUI components.
             </Typography>
-          </Stack>
+          </Stack> */}
           <Box sx={{ display: 'flex', justifyContent: 'center' }}>
             <Box
               component="img"
               alt="Widgets"
-              src="/assets/auth-widgets.png"
-              sx={{ height: 'auto', width: '100%', maxWidth: '600px' }}
+              src="/assets/Authentication.png"
+              sx={{ height: 'auto', width: 'auto',  }}
             />
           </Box>
         </Stack>
